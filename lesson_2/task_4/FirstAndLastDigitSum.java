7d7fe5758a8bf635e44fa60c89c37630fdcff035
IlyaCool <frezhgrand@gmail.com>
2021-11-15 00:55:20 +0300

    make les2_tk4

--- lesson_2/task_4/FirstAndLastDigitSum.java
+++ lesson_2/task_4/FirstAndLastDigitSum.java
@@ -1,5 +1,20 @@
 public class FirstAndLastDigitSum {
-	public static int calcSum() {
+	public static int calcSum(int number) {
+		if (number < 0) 
+			return -1;
 
+		int digit = 10;
+		if (number < 10)
+			return number;
+
+		while (number / digit > 10)
+			digit *= 10;
+
+		// 100000000
+		// 12.3456789
+		int firstDigit = number / digit;
+		int lastDigit = number % 10;
+
+		return firstDigit + lastDigit;
 	}
 }
---
+++ lesson_2/task_4/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+	public static void main(String[] args) {
+		int digit = FirstAndLastDigitSum.calcSum(35558);
+		System.out.println(digit);
+	}
+}
