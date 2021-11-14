1babd58a3d962d605532faaab9c90b05afe4a275
IlyaCool <frezhgrand@gmail.com>
2021-11-15 01:41:29 +0300

    make les2_tk6

---
+++ lesson_2/task_6/SharedDigit.java
@@ -0,0 +1,15 @@
+public class SharedDigit {
+	public static boolean hasSharedDigit(int a, int b) {
+		if (a < 10 || a > 99 || b < 10 || b > 99)
+			return false;
+
+		int[] aDigits = { a % 10, a / 10 % 10 };
+		int[] bDigits = { b % 10, b / 10 % 10 };
+		for (int aDigit : aDigits)
+			for (int bDigit : bDigits)
+				if (aDigit == bDigit)
+					return true;
+
+		return false;
+	}
+}
