f63516220da48edb0093cd077d6366ec6d9b10ba
IlyaCool <frezhgrand@gmail.com>
2021-11-14 18:19:36 +0300

    l2t1edited

--- lesson_2/task_1/Main.java
+++ lesson_2/task_1/Main.java
@@ -1,10 +1,5 @@
 class Main {
 	public static void main(String[] args) {
-		String[] argvs = {
-			"String"
-		};	
-	
-		for (String str : argvs)
-			System.out.println(str);
+		NumberPalindrome.isPalindrome(2021);
 	}
 }
--- lesson_2/task_1/NumberPalindrome.java
+++ lesson_2/task_1/NumberPalindrome.java
@@ -1,6 +1,8 @@
 public class NumberPalindrome {
 	public static boolean isPalindrome(int number) {
+		String numberStr = Integer.toString(number);
 		
+
 		return false;
 	}
 }
