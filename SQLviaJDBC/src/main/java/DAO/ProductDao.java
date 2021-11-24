a13aef578705815eaaf3a02695aa5dcf6d7852b5
iia.m.y <frezhgrand@gmail.com>
2021-11-25 01:05:02 +0300

    make: complete ProductDao

--- SQLviaJDBC/src/main/java/DAO/ProductDao.java
+++ SQLviaJDBC/src/main/java/DAO/ProductDao.java
@@ -11,6 +11,9 @@ public class ProductDao {
 
     public static final String SELECT_ALL_PRODUCTS = "SELECT * FROM products";
     public static final String SELECT_BY_PRODUCT_ID = "SELECT * FROM products WHERE id=?";
+    public static final String INSERT_PRODUCT = "INSERT INTO products(`ProductName`, `Price`) VALUES (?, ?)";
+    public static final String UPDATE_PRODUCT = "UPDATE products SET `ProductName`=?,`Price`=? WHERE Id=?";
+    public static final String DELETE_PRODUCT = "DELETE FROM products WHERE Id=?";
 
     public static Product get(int id) {
         return id > 0 ? getProductByID(id) : new Product();
@@ -39,25 +42,51 @@ public class ProductDao {
         try (Connection connection = ConnectionManager.open();
             PreparedStatement statement = connection.prepareStatement(SELECT_ALL_PRODUCTS)) {
             ResultSet result = statement.executeQuery();
-            while (result.next()) {
-                allProducts.add(getFromResult(result));
-            }
+            return result.next() ? getListFromResult(result) : new ArrayList<>(){{add(new Product());}};
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        return allProducts;
+    }
+
+    private static List<Product> getListFromResult(ResultSet result) throws SQLException {
+        List<Product> resultList = new ArrayList<>();
+        do {
+            resultList.add(getFromResult(result));
+        } while (result.next());
+        return resultList;
     }
 
     public static void save(Product product) {
-        throw new UnsupportedOperationException();
+        try (Connection connection = ConnectionManager.open();
+            PreparedStatement statement = connection.prepareStatement(INSERT_PRODUCT)) {
+            statement.setString(1, product.getProductName());
+            statement.setInt(2, product.getPrice());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public static void update(Product product, String[] params) {
-        throw new UnsupportedOperationException();
+    public static void update(int id, String[] params) {
+        try (Connection connection = ConnectionManager.open();
+            PreparedStatement statement = connection.prepareStatement(UPDATE_PRODUCT)) {
+            statement.setString(1, params[0]);
+            statement.setInt(2, Integer.parseInt(params[1]));
+            statement.setInt(3, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public static void delete(Product product) {
-        throw new UnsupportedOperationException();
+    public static void delete(int id) {
+        try (Connection connection = ConnectionManager.open();
+            PreparedStatement statement = connection.prepareStatement(DELETE_PRODUCT)) {
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     private ProductDao() {}
--- SQLviaJDBC/src/main/java/Main.java
+++ SQLviaJDBC/src/main/java/Main.java
@@ -3,5 +3,7 @@ import DAO.ProductDao;
 public class Main {
     public static void main(String[] args) {
         System.out.println(ProductDao.getAll());
+        ProductDao.delete(10);
+        System.out.println(ProductDao.getAll());
     }
 }
