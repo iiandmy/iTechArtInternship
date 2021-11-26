cf5488f57f4d87a6b85ab519954cc5a818a5dcfe
iia.m.y <frezhgrand@gmail.com>
2021-11-26 11:34:47 +0300

    edit: problems fix, update hibernate version

--- SQLviaHibernate/src/main/java/Main.java
+++ SQLviaHibernate/src/main/java/Main.java
@@ -4,7 +4,7 @@ import entities.Product;
 public class Main {
     public static void main(String[] args) {
         System.out.println(ProductDao.findAll());
-        ProductDao.save(new Product("Fuji 65", 500));
+        ProductDao.save(new Product("Apple Magic Board", 300));
         System.out.println(ProductDao.findAll());
     }
 }
